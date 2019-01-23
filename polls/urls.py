de8808efd9297993990e76892d659cca361daefc
MuhammadTausif <tausifasia@gmail.com>
2019-01-23 13:22:21 +0500

    nlp_files added

--- polls/urls.py
+++ polls/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('1', views.index, name='index'),
+    path('2', views.index1, name='index'),
+    path('2', views.index2, name='index'),
+    path('3', views.index3, name="good")
 ]
--- polls/views.py
+++ polls/views.py
@@ -3,6 +3,14 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
-
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("This is again a request")
+
+def index1(request):
+    return HttpResponse("1")
+
+def index2(request):
+    return HttpResponse("2")
+
+def index3(request):
+    return HttpResponse("3++")
